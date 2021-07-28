b1089dc0c0660aea1256ee780f2e057e98006c40
snaiper04ek <snaiper04ek353@gmail.com>
2021-07-28 15:13:16 +0300

    added files License and settings for main code.

---
+++ settings.py
@@ -0,0 +1,7 @@
+ftpserver = 'ftp.example.com' #adress of FTP server
+ftplogin = 'ftpuser1example' #login for FTP server
+ftppassword = 'ftppassword1example' #password for FTP server
+pricefile = '\\\SERVER\\examplenetworkpath\\myfiletoupload.pdf' #path to the FILE
+archpath = 'C:\\myfolder\\examplepathtofolder\\archive FOLDER' #archive FOLDER
+ftppath = '/example_catalog/myfileuploaded.pdf' #path on ftpserver to FOLDER where need upload pfile
+sleep = 4 #time to sleep between checks
